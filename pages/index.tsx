d66a21460f052ac8b8dda2f23d82b539774f5c41
Steve Larsen <slarsen-3@att.net>
2023-06-03 12:58:51 -0500

    added images & quote generator elements
    including styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,29 @@
+import React, { useState } from 'react';
+
 import Head from 'next/head';
 import Image from 'next/image';
-import { GradientBackgroundContainer } from './components/QuoteGenerator/QuoteGeneratorElements';
+
+// Components
+import {
+  CloudThunder,
+  CloudyWeather,
+  FooterContainer,
+  FooterLink,
+  GenerateQuoteButton,
+  GenerateQuoteButtonText,
+  GradientBackgroundContainer,
+  QuoteGeneratorContainer,
+  QuoteGeneratorInnerContainer,
+  QuoteGeneratorSubTitle,
+  QuoteGeneratorTitle,
+} from './components/QuoteGenerator/QuoteGeneratorElements';
+
+// Assets
+import cloudyWeather from '@/assets/cloudy-weather.png';
+import cloudThunder from '@/assets/cloud-and-thunder.png';
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
   return (
     <>
       <Head>
@@ -15,7 +36,46 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       {/* Background */}
-      <GradientBackgroundContainer></GradientBackgroundContainer>
+      <GradientBackgroundContainer>
+        {/* Quote Generator */}
+        <QuoteGeneratorContainer>
+          <QuoteGeneratorInnerContainer>
+            <QuoteGeneratorTitle>Daily Quote</QuoteGeneratorTitle>
+            <QuoteGeneratorSubTitle>
+              Generate a quote card with a random inspirational quote provided
+              by the{' '}
+              <FooterLink
+                href='https://zenquotes.io/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                ZenQuotes API
+              </FooterLink>
+              .
+            </QuoteGeneratorSubTitle>
+
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText>Make a Quote</GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+          </QuoteGeneratorInnerContainer>
+        </QuoteGeneratorContainer>
+
+        <CloudyWeather src={cloudyWeather} height='300' alt='cloudy-weather' />
+        <CloudThunder src={cloudThunder} height='300' alt='cloud-and-thunder' />
+
+        {/* Footer */}
+        <FooterContainer>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed by{' '}
+            <FooterLink href='' target='_blank' rel='noopener noreferrer'>
+              {' '}
+              Steve Larsen.
+            </FooterLink>
+          </>
+        </FooterContainer>
+      </GradientBackgroundContainer>
     </>
   );
 }
